c22a8ec4a3fcbbd7e2e536afcfb87b159f9c732e
Thomas.Donovan <Thomas.Donovan@42513187-17f6-4ddd-9b82-9478f1c77761>
2008-08-02 17:51:29 +0000

    use connection config instead of connection notes, retain env vars with query results

--- trunk/mod_log_dbd.c
+++ trunk/mod_log_dbd.c
@@ -1,6 +1,6 @@
-/*  MOD_LOG_DBD     Apache 2.3+ module
+/*  MOD_LOG_DBD     Apache 2.2 module
 
-Copyright 2007  Tom Donovan
+Copyright 2008  Tom Donovan
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
